0fe0c26814f67814e4c1941e62a9ae23e5c1d9cd
Drew K <amackzie@gmail.com>
2026-06-16 23:18:22 +0300

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.ts
@@ -0,0 +1,17 @@
+class Solution {
+    mergeTwoLists(list1, list2) {
+        if (!list1) {
+            return list2;
+        }
+        if (!list2) {
+            return list1;
+        }
+        if (list1.val <= list2.val) {
+            list1.next = this.mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = this.mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
