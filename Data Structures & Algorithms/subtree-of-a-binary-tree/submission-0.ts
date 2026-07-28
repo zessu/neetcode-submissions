15d060a0bd94412fcbed9054aa469f13d9ac14c2
Drew K <amackzie@gmail.com>
2026-07-28 10:28:37 +0300

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+    isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+        let isSubTree = false;
+
+        const compareTrees = (root: TreeNode | null, subRoot: TreeNode | null) => {
+            if(!root && !subRoot) return true;
+            if(root && subRoot && root.val === subRoot.val) {
+                return compareTrees(root.left, subRoot.left) && compareTrees(root.right, subRoot.right)
+            }
+            else return false;
+        }
+
+        const traverseTree = (root: TreeNode | null) => {
+            if(root === null) return;
+            if(root.val === subRoot.val) {
+                const res = compareTrees(root, subRoot);
+                if(res) isSubTree = true;
+            };
+            traverseTree(root.left);
+            traverseTree(root.right);
+            return;
+        }
+
+        traverseTree(root);
+
+        return isSubTree;
+    }
+}
