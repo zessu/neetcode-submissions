ee83c4090f10ee5e3a48a7ba598de03bbfbab97b
Drew K <amackzie@gmail.com>
2026-08-27 10:37:17 +0300

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums: number[], val: number): number {
+        for(let count=0; count<nums.length; count++) {
+            if(nums[count] === val) {
+                nums.splice(count, 1, Infinity);
+            }
+        }
+        nums = nums.sort();
+        return nums.reduce((acc, curr) => acc += curr === Infinity ? 0: 1, 0);
+    }
+}
